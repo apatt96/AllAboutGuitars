b516d0cd6770e0ec70c17d969cc16ee1bc34e933
Andrew Patterson <andrewpatterson96@gmail.com>
2018-02-05 13:07:01 -0500

    continuous link fixes

--- js/guitars.js
+++ js/guitars.js
@@ -12,10 +12,9 @@ $(document).ready(function () {
 });
 
 /*HOME PAGE ANIMATIONS*/
-alert(window.location.pathname);
 //$(window).scroll(function() {
 if ($(window).width() > 800) {
-    if ( window.location.pathname == "/AllAboutGuitars"){
+    if ( window.location.pathname == "/AllAboutGuitars/index.html"){
         $(".header").hide();
         var topofDiv = $(".hero-home").offset().top; 
         var height = $(".hero-home").outerHeight(); 
@@ -34,7 +33,7 @@ if ($(window).width() > 800) {
 }
 
 /*Smooth Scroll*/
-if ( window.location.pathname == "/AllAboutGuitars"){
+if ( window.location.pathname == "/AllAboutGuitars/index.html"){
     $("#scroller").click(function () {
         $("html, body").animate({
             scrollTop: 0
